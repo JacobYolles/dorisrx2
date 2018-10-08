09ee5571266f651ad2b28e82d577a564751dba6e
Jordan Schechter <jordanwschechter@gmail.com>
2018-10-08 15:27:27 -0600

    nav bar

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs/NavTabs";
+import Authentication from "./pages/Authentication";
+import User from "./pages/User";
+import Caregiver from "./pages/Caregiver";
+import Input from "./pages/Input";
+import Reports from "./pages/Reports";
+
+
+
+
+const App = () => (
+  <Router>
+    <div>
+      <NavTabs />
+      <Route exact path="/" component={Authentication} />
+      <Route path="/user" component={User} />
+      <Route path="/caregiver" component={Caregiver} />
+      <Route exact path="/input" component={Input} />
+      <Route exact path="/reports" component={Reports} />
+    </div>
+  </Router>
+);
+
+
+
+
+
+export default App;
