8949adc9bd93e00e2f26fe92fcda30bd85f2d995
JacobYolles <jacob.yolles@ucdenver.edu>
2018-10-08 15:00:42 -0600

    added correction

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const drugRoutes = require("./drug");
 const contactRoutes = require("./contact")
+const inventoryRoutes = require("./Inventory")
 
 // All routes
 router.use("/drugs", drugRoutes);
 router.use("/contact", contactRoutes)
+router.use("/inventory", inventoryRoutes)
 
 module.exports = router;
