94915f7b8a380618923cce77c30b1ab3b66cc4b6
JacobYolles <jacob.yolles@ucdenver.edu>
2018-10-07 18:42:12 -0600

    added schema values and creation

---
+++ server.js
@@ -0,0 +1,38 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+
+// Initialize Express
+var app = express();
+
+// Set up a static folder (public) for our web app
+app.use(express.static("public"));
+
+// Database configuration
+// Save the URL of our database as well as the name of our collection
+var databaseUrl = "drugs";
+var collections = ["specifics"];
+
+// Use mongojs to hook the database to the db variable
+var db = mongojs(databaseUrl, collections);
+
+// This makes sure that any errors are logged if mongodb runs into an issue
+db.on("error", function(error) {
+    console.log("Database Error:", error);
+  });
+
+  
+  // 2. At the "/all" path, display every entry in the animals collection
+app.get("/all", function(req, res) {
+    // Query: In our database, go to the animals collection, then "find" everything
+    db.specifics.find({}, function(error, found) {
+      // Log any errors if the server encounters one
+      if (error) {
+        console.log(error);
+      }
+      // Otherwise, send the result of this query to the browser
+      else {
+        res.json(found);
+      }
+    });
+  });
