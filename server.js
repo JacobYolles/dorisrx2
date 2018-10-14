c9362d3bf7b6cbc0175a2cea45ce4e5be93a9190
JacobYolles <jacob.yolles@ucdenver.edu>
2018-10-13 23:17:12 -0600

    pushed pull

--- server.js
+++ server.js
@@ -3,51 +3,12 @@ const express = require("express")
 const routes = require("./routes");
 const mongoose = require("mongoose")
 var app = express();
-// Configuring Passport
-var passport = require('passport');
-var expressSession = require('express-session');
-app.use(expressSession({secret: 'mySecretKey'}));
-app.use(passport.initialize());
-app.use(passport.session());
-var dbConfig = require('./db.js');
-mongoose.connect(dbConfig.url);
 // app.use(express.static("public"));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
-
-
-
-
-
-
-// Initialize Passport
-var initPassport = require('./passport/init');
-initPassport(passport);
-
-
-app.use('/', routes);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
 }
 
 app.use(routes);
