84961cf9d4621ee7e620c4971b7a82a01f3458b8
JacobYolles <jacob.yolles@ucdenver.edu>
2018-10-13 23:04:05 -0600

    prepull

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(passport.session());
 var dbConfig = require('./db.js');
 mongoose.connect(dbConfig.url);
 // app.use(express.static("public"));
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 
 if (process.env.NODE_ENV === "production") {
@@ -20,14 +20,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-var flash = require('connect-flash');
-app.use(flash());
+
+
 
 // Initialize Passport
 var initPassport = require('./passport/init');
