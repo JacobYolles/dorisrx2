c2f88887b032053fc12a42ca448fbe0e63c1df6c
JacobYolles <jacob.yolles@ucdenver.edu>
2018-10-14 17:16:47 -0600

    added data

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,7 +4,7 @@ const drugRoutes = require("./Drug");
 const fdaRoutes = require("./Fda")
 const inventoryRoutes = require("./Inventory")
 const userRoutes = require("./User")
-var express = require('express');
+// var express = require('express');
 // All routes
 router.use("/contact", contactRoutes)
 router.use("/drug", drugRoutes)
