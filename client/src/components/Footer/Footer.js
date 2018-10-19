d0e4e7602d709cf3ed2027a0022e6551ccf40ecf
JacobYolles <jacob.yolles@ucdenver.edu>
2018-10-18 23:28:06 -0600

    fixed footer issue for heroku

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -6,11 +6,7 @@ const Footer = () => (
         <div className="bottom">
             <ul id='footer-list'>
                 <li><a href="https://github.com/tomkent321/dorisrx" target="blank"><span><img className='logo' src="assets/images/github.png"/></span> Github</a></li>
-<<<<<<< HEAD
                 <li><a href="https://docs.google.com/document/d/1hmJwI8VdhmWwwZAqUk8WRA-iKPNAXHpNeaWE01SHs10/edit?usp=sharing" target="blank">© 2018 <span class="font">DorisRx</span></a></li>
-=======
-                <li>© 2018 <span id="font">DorisRx</span></li>
->>>>>>> 0fdcc931fa42402a077ad8942c64fe4a91eea21d
                 <li><span><img className='logo' id="reactLogo" src="assets/images/react-logo.png"/></span> Powered By React</li>
                 
 
